4e621aca6d5833b37efbee25d52eea5aabea0fe6
lwonho92 <lwonho92@gmail.com>
2017-01-12 19:33:47 +0900

    Complete S10.03.
    Add FirebaseJobDispatcher.

--- Sunshine/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -7,15 +7,50 @@ import android.net.Uri;
 import android.os.AsyncTask;
 
 import com.example.android.sunshine.data.WeatherContract.WeatherEntry;
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.Driver;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.Trigger;
+
+import java.util.concurrent.TimeUnit;
 
 public class SunshineSyncUtils {
+
+    private static final int SYNC_INTERVAL_HOURS = 3;
+    private static final int SYNC_INTERVAL_SECONDS = (int) TimeUnit.HOURS.toSeconds(SYNC_INTERVAL_HOURS);
+    private static final int SYNC_FLEXTIME_SECONDS = SYNC_INTERVAL_SECONDS / 3;
+
     private static boolean sInitialized;
+
+    private static final String SYNC_JOB_TAG = "sync_job_tag";
+
+    public static void schedulePeriodid(final Context context) {
+        Driver driver = new GooglePlayDriver(context);
+        FirebaseJobDispatcher firebaseJobDispatcher = new FirebaseJobDispatcher(driver);
+
+        Job syncJob = firebaseJobDispatcher.newJobBuilder()
+                .setService(SunshineFirebaseJobService.class)
+                .setTag(SYNC_JOB_TAG)
+                .setConstraints(Constraint.ON_ANY_NETWORK)
+                .setLifetime(Lifetime.FOREVER)
+                .setRecurring(true)
+                .setTrigger(Trigger.executionWindow(
+                        SYNC_INTERVAL_SECONDS,
+                        SYNC_INTERVAL_SECONDS + SYNC_FLEXTIME_SECONDS))
+                .setReplaceCurrent(true)
+                .build();
+    }
 
     synchronized public static void initialize(final Context context) {
 
         if(sInitialized)
             return;
         sInitialized = true;
+
+        schedulePeriodid(context);
 
         new AsyncTask<Void, Void, Void>() {
             @Override
@@ -35,7 +70,6 @@ public class SunshineSyncUtils {
             }
         }.execute();
     }
-
 
     public static void startImmediateSync(final Context context) {
         Intent intent = new Intent(context, SunshineSyncIntentService.class);
