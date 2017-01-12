159ecf074fa22415d2132474da3468afb4cd959a
lwonho92 <lwonho92@gmail.com>
2017-01-12 20:41:51 +0900

    Complete S10.04.
    Add Period Notification.

--- Sunshine/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -3,9 +3,12 @@ package com.example.android.sunshine.sync;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
+import android.text.format.DateUtils;
 
+import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.data.WeatherContract.WeatherEntry;
 import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.NotificationUtils;
 import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
 
 import java.net.URL;
@@ -29,6 +32,19 @@ public class SunshineSyncTask {
                 contentResolver.delete(WeatherEntry.CONTENT_URI, null, null);
 
                 contentResolver.bulkInsert(WeatherEntry.CONTENT_URI, simpleJsonWeatherData);
+
+                boolean notificationsEnabled = SunshinePreferences.areNotificationsEnabled(context);
+
+                long timeSinceLastNotification = SunshinePreferences.getEllapsedTimeSinceLastNotification(context);
+                boolean oneDayPassedSinceLastNotification = false;
+
+                if (timeSinceLastNotification >= DateUtils.DAY_IN_MILLIS) {
+                    oneDayPassedSinceLastNotification = true;
+                }
+
+                if (notificationsEnabled && oneDayPassedSinceLastNotification) {
+                    NotificationUtils.notifyUserOfNewWeather(context);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
---
+++ Sunshine/app/src/main/java/com/example/android/sunshine/utilities/NotificationUtils.java
@@ -0,0 +1,105 @@
+package com.example.android.sunshine.utilities;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.content.ContextCompat;
+
+import com.example.android.sunshine.DetailActivity;
+import com.example.android.sunshine.R;
+import com.example.android.sunshine.data.SunshinePreferences;
+import com.example.android.sunshine.data.WeatherContract;
+
+public class NotificationUtils {
+
+    public static final String[] WEATHER_NOTIFICATION_PROJECTION = {
+            WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+    };
+
+    public static final int INDEX_WEATHER_ID = 0;
+    public static final int INDEX_MAX_TEMP = 1;
+    public static final int INDEX_MIN_TEMP = 2;
+
+    public static final int NOTIFICATION_ID = 1359;
+
+    public static void notifyUserOfNewWeather(Context context) {
+        Uri todaysWeatherUri = WeatherContract.WeatherEntry
+                .buildWeatherUriWithDate(SunshineDateUtils.normalizeDate(System.currentTimeMillis()));
+
+        Cursor todayWeatherCursor = context.getContentResolver().query(
+                todaysWeatherUri,
+                WEATHER_NOTIFICATION_PROJECTION,
+                null,
+                null,
+                null);
+
+        if (todayWeatherCursor.moveToFirst()) {
+            int weatherId = todayWeatherCursor.getInt(INDEX_WEATHER_ID);
+            double high = todayWeatherCursor.getDouble(INDEX_MAX_TEMP);
+            double low = todayWeatherCursor.getDouble(INDEX_MIN_TEMP);
+
+            Resources resources = context.getResources();
+            int largeArtResourceId = SunshineWeatherUtils
+                    .getLargeArtResourceIdForWeatherCondition(weatherId);
+
+            Bitmap largeIcon = BitmapFactory.decodeResource(
+                    resources,
+                    largeArtResourceId);
+
+            String notificationTitle = context.getString(R.string.app_name);
+
+            String notificationText = getNotificationText(context, weatherId, high, low);
+
+            /* getSmallArtResourceIdForWeatherCondition returns the proper art to show given an ID */
+            int smallArtResourceId = SunshineWeatherUtils
+                    .getSmallArtResourceIdForWeatherCondition(weatherId);
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                    .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                    .setSmallIcon(smallArtResourceId)
+                    .setLargeIcon(largeIcon)
+                    .setContentTitle(notificationTitle)
+                    .setContentText(notificationText)
+                    .setAutoCancel(true);
+
+            Intent intent = new Intent(context, DetailActivity.class);
+            intent.setData(todaysWeatherUri);
+
+            TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
+            taskStackBuilder.addNextIntentWithParentStack(intent);
+            PendingIntent pendingIntent = taskStackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            builder.setContentIntent(pendingIntent);
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(NOTIFICATION_ID, builder.build());
+
+            SunshinePreferences.saveLastNotificationTime(context, System.currentTimeMillis());
+        }
+
+        todayWeatherCursor.close();
+    }
+
+    private static String getNotificationText(Context context, int weatherId, double high, double low) {
+
+        String shortDescription = SunshineWeatherUtils.getStringForWeatherCondition(context, weatherId);
+
+        String notificationFormat = context.getString(R.string.format_notification);
+
+        String notificationText = String.format(notificationFormat,
+                shortDescription,
+                SunshineWeatherUtils.formatTemperature(context, high),
+                SunshineWeatherUtils.formatTemperature(context, low));
+
+        return notificationText;
+    }
+}
