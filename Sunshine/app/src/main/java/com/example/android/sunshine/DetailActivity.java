f1c3f87cffdf79cb9249c4cb8e14e56d65c8283c
lwonho92 <lwonho92@gmail.com>
2017-02-11 14:15:11 +0900

    Docs init.

--- Sunshine/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -12,7 +12,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.android.sunshine.data.WeatherContract;
 import com.example.android.sunshine.data.WeatherContract.WeatherEntry;
